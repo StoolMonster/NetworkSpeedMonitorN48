2ba45f83f615320ccebed5c7d40601ad68acacef
StoolMonster <lfw93814@gmail.com>
2024-08-12 21:46:59 +0800

    Add project files.

---
+++ NetworkSpeedMonitorN48/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace NetworkSpeedMonitorN48
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelWiFi = new System.Windows.Forms.Label();
+            this.labelEth = new System.Windows.Forms.Label();
+            this.labelWiFiSe = new System.Windows.Forms.Label();
+            this.labelEthSe = new System.Windows.Forms.Label();
+            this.labelWiFiRe = new System.Windows.Forms.Label();
+            this.labelEthRe = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.keepOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetWindowPositionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelWiFi
+            // 
+            this.labelWiFi.Location = new System.Drawing.Point(0, 0);
+            this.labelWiFi.Name = "labelWiFi";
+            this.labelWiFi.Size = new System.Drawing.Size(40, 20);
+            this.labelWiFi.TabIndex = 0;
+            this.labelWiFi.Text = "WiFi";
+            // 
+            // labelEth
+            // 
+            this.labelEth.Location = new System.Drawing.Point(0, 20);
+            this.labelEth.Name = "labelEth";
+            this.labelEth.Size = new System.Drawing.Size(40, 20);
+            this.labelEth.TabIndex = 1;
+            this.labelEth.Text = "Eth";
+            // 
+            // labelWiFiSe
+            // 
+            this.labelWiFiSe.Location = new System.Drawing.Point(46, 0);
+            this.labelWiFiSe.Name = "labelWiFiSe";
+            this.labelWiFiSe.Size = new System.Drawing.Size(80, 20);
+            this.labelWiFiSe.TabIndex = 2;
+            this.labelWiFiSe.Text = "0 KB/s";
+            this.labelWiFiSe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelEthSe
+            // 
+            this.labelEthSe.Location = new System.Drawing.Point(46, 20);
+            this.labelEthSe.Name = "labelEthSe";
+            this.labelEthSe.Size = new System.Drawing.Size(80, 20);
+            this.labelEthSe.TabIndex = 3;
+            this.labelEthSe.Text = "0 KB/s";
+            this.labelEthSe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelWiFiRe
+            // 
+            this.labelWiFiRe.Location = new System.Drawing.Point(132, 0);
+            this.labelWiFiRe.Name = "labelWiFiRe";
+            this.labelWiFiRe.Size = new System.Drawing.Size(80, 20);
+            this.labelWiFiRe.TabIndex = 4;
+            this.labelWiFiRe.Text = "0 KB/s";
+            this.labelWiFiRe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelEthRe
+            // 
+            this.labelEthRe.Location = new System.Drawing.Point(132, 20);
+            this.labelEthRe.Name = "labelEthRe";
+            this.labelEthRe.Size = new System.Drawing.Size(80, 20);
+            this.labelEthRe.TabIndex = 5;
+            this.labelEthRe.Text = "0 KB/s";
+            this.labelEthRe.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.keepOnTopToolStripMenuItem,
+            this.resetWindowPositionToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(230, 76);
+            // 
+            // keepOnTopToolStripMenuItem
+            // 
+            this.keepOnTopToolStripMenuItem.Name = "keepOnTopToolStripMenuItem";
+            this.keepOnTopToolStripMenuItem.Size = new System.Drawing.Size(229, 24);
+            this.keepOnTopToolStripMenuItem.Text = "Disable Keep On Top";
+            this.keepOnTopToolStripMenuItem.Click += new System.EventHandler(this.keepOnTopToolStripMenuItem_Click);
+            // 
+            // resetWindowPositionToolStripMenuItem
+            // 
+            this.resetWindowPositionToolStripMenuItem.Name = "resetWindowPositionToolStripMenuItem";
+            this.resetWindowPositionToolStripMenuItem.Size = new System.Drawing.Size(229, 24);
+            this.resetWindowPositionToolStripMenuItem.Text = "Reset Window Position";
+            this.resetWindowPositionToolStripMenuItem.Click += new System.EventHandler(this.resetPositionToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(229, 24);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(241, 81);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.labelEthRe);
+            this.Controls.Add(this.labelWiFiRe);
+            this.Controls.Add(this.labelEthSe);
+            this.Controls.Add(this.labelWiFiSe);
+            this.Controls.Add(this.labelEth);
+            this.Controls.Add(this.labelWiFi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWiFi;
+        private System.Windows.Forms.Label labelEth;
+        private System.Windows.Forms.Label labelWiFiSe;
+        private System.Windows.Forms.Label labelEthSe;
+        private System.Windows.Forms.Label labelWiFiRe;
+        private System.Windows.Forms.Label labelEthRe;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem keepOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetWindowPositionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+    }
+}
+
---
+++ NetworkSpeedMonitorN48/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkSpeedMonitorN48
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounterCategory category;
+        private String[] instanceNames;
+        private PerformanceCounter[] uploadCounters;
+        private PerformanceCounter[] downloadCounters;
+        private System.Windows.Forms.Timer timer;
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        private int line1NI = 0;
+        private int line2NI = 0;
+
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+        private bool mouseInteractionEnabled = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.TopMost = true;
+
+            ResetWindowPosition();
+
+            //
+            labelWiFi.MouseDown += Form1_MouseDown;
+            labelWiFiSe.MouseDown += Form1_MouseDown;
+            labelWiFiRe.MouseDown += Form1_MouseDown;
+            labelEth.MouseDown += Form1_MouseDown;
+            labelEthSe.MouseDown += Form1_MouseDown;
+            labelEthRe.MouseDown += Form1_MouseDown;
+
+            // Initialize network interfaces
+            category = new PerformanceCounterCategory("Network Interface");
+            instanceNames = category.GetInstanceNames();
+            uploadCounters = new PerformanceCounter[instanceNames.Length];
+            downloadCounters = new PerformanceCounter[instanceNames.Length];
+
+            // Set up PerformaceCounters for each network interface
+            for (int i = 0; i < instanceNames.Length; i++)
+            {
+                string instanceName = instanceNames[i];
+
+                uploadCounters[i] = new PerformanceCounter("Network Interface", "Bytes Sent/sec", instanceName);
+                downloadCounters[i] = new PerformanceCounter("Network Interface", "Bytes Received/sec", instanceName);
+            }
+
+            // Set up ContextMenuStrip to choose which network interface to show.
+            contextMenuStrip2.Opening += ContextMenuStrip2_Opening;
+            labelWiFi.ContextMenuStrip = contextMenuStrip2;
+            labelEth.ContextMenuStrip = contextMenuStrip2;
+
+            // Set up tray icon
+            trayIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application,
+                Text = "NetworkSpeedMonitor",
+                Visible = true
+            };
+
+            // Set up tray menu
+            trayMenu = new ContextMenuStrip();
+            ToolStripMenuItem toggleMouseInteractionItem = new ToolStripMenuItem("Toggle Mouse Interaction", null, ToggleMouseInteraction_Click);
+            trayMenu.Items.Add(toggleMouseInteractionItem);
+            trayIcon.ContextMenuStrip = trayMenu;
+
+            // Set up update timer
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void ToggleMouseInteraction_Click(object sender, EventArgs e)
+        {
+            mouseInteractionEnabled = !mouseInteractionEnabled;
+            this.Enabled = mouseInteractionEnabled;
+            this.Text = mouseInteractionEnabled ? "Mouse Interaction Enabled" : "Mouse Interaction Disabled";
+        }
+
+        private void ContextMenuStrip2_Opening(object sender, EventArgs e)
+        {
+            contextMenuStrip2.Items.Clear();
+
+            for (int i = 0; i < instanceNames.Length; i++)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem(instanceNames[i]);
+                item.Tag = instanceNames[i];
+                item.Click += Item_Click;
+                contextMenuStrip2.Items.Add(item);
+            }
+        }
+
+        private void Item_Click(object sender, EventArgs e)
+        {
+            var selectedItem = (ToolStripMenuItem)sender;
+            int selectedInterfaceIndex = contextMenuStrip2.Items.IndexOf(selectedItem);
+
+            if (contextMenuStrip2.SourceControl == labelWiFi)
+            {
+                line1NI = selectedInterfaceIndex;
+            }
+            else if (contextMenuStrip2.SourceControl == labelEth)
+            {
+                line2NI = selectedInterfaceIndex;
+            }
+        }
+
+        private void ResetWindowPosition()
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            int x = 0;
+            int y = Screen.PrimaryScreen.Bounds.Height - this.Height;
+            this.Location = new Point(x, y);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string uploadSpeed = FormatBytes(uploadCounters[line1NI].NextValue());
+            string downloadSpeed = FormatBytes(downloadCounters[line1NI].NextValue());
+            labelWiFiSe.Text = uploadSpeed + "/s";
+            labelWiFiRe.Text = downloadSpeed + "/s";
+
+            uploadSpeed = FormatBytes(uploadCounters[line2NI].NextValue());
+            downloadSpeed = FormatBytes(downloadCounters[line2NI].NextValue());
+            labelEthSe.Text = uploadSpeed + "/s";
+            labelEthRe.Text = downloadSpeed + "/s";
+        }
+
+        private string FormatBytes(float bytes)
+        {
+            bytes /= 1024;
+            string[] sizes = { "KB", "MB", "GB", "TB" };
+            int order = 0;
+            while (bytes >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                bytes /= 1024;
+            }
+            return String.Format("{0:0.#} {1}", bytes, sizes[order]);
+        }
+
+
+        // Use mouse left button to move the form
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void keepOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+            if (this.TopMost)
+            {
+                keepOnTopToolStripMenuItem.Text = "Disbale Keep on Top";
+            }
+            else
+            {
+                keepOnTopToolStripMenuItem.Text = "Keep on Top";
+
+            }
+        }
+
+        private void resetPositionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetWindowPosition();
+        }
+    }
+}
